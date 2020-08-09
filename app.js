dc6075962323aa89c7bad7e7c3c6c695f83bebd3
Allen <allenvs@mail.ru>
2020-08-09 07:33:08 +0300

    added arrayToList as well

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-function listToArray(head) { 
-    let array = [];
-    for (current = head; current != null; current = current.next) {
-      array.push(current.value);
-    }
-    return array;
-  }
+function arrayToList(array) { 
+    if (array.length == 0) { return null; }
+    let list = {
+        value: array[0],
+        next: null
+    };
+    let previous = list;
+    for(let i = 1; i < array.length; i++) { 
+        previous.next = { 
+            value: array[i],
+            next: null
+        };
+        previous = previous.next;
+    };
+    return list;
+};
   
-  console.log(listToArray(arrayToList([10, 20, 30])));
-  // → [10, 20, 30]
+function listToArray(list) { 
+    let array = [];
+    // this is next level magic right here
+    for (current = list; current != null; current = current.next) {
+        array.push(current.value);
+    };
+    return array;
+};
+
+console.log(arrayToList([10, 20, 30]));
+// → {value: 10, rest: {value: 20, rest: null}}
+
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
